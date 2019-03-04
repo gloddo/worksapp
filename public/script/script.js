30126f46d13e4a21385b49f95e79beaa4872434e
Joe Makkio <makkio94@gmail.com>
2019-03-04 17:12:00 +0100

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,23 +1,24 @@
 $(document).ready(() => {
-  $(".chat").scrollTop(100000)
-  $("#menu-btn").on("click", () => {
-    $("aside").toggle();
-  })
+	$(".chat").scrollTop(100000)
+
+	$("#menu-btn").on("click", () => {
+		$("aside").toggle();
+	})
 })
 
 
 function sendMessage(event) {
-  let c = 0;
-  if (event.which == 13 && event.target.value) {
-    c++
-    var time = new Date().toLocaleTimeString('en-GB', {
-      hour12: false,
-      hour: "numeric",
-      minute: "numeric"
-    });
-    var date = new Date().toLocaleDateString();
-    $(".chat").append(
-      `<article class="chat-row" id=s${c}>
+	let c = 0;
+	if (event.which == 13 && event.target.value) {
+		c++
+		var time = new Date().toLocaleTimeString('en-GB', {
+			hour12: false,
+			hour: "numeric",
+			minute: "numeric"
+		});
+		var date = new Date().toLocaleDateString();
+		$(".chat").append(
+			`<article class="chat-row" id=s${c}>
         <span class="message">
           ${event.target.value}
         </span>
@@ -30,9 +31,9 @@ function sendMessage(event) {
           </div>
         </span>
       </article>`)
-    event.target.value = ""
-    $(".unseen").removeClass("unseen")
-    //  $(".chat").scrollTop($(".chat")[0].scrollHeight)
-    $(".chat").scrollTop(10000)
-  }
+		event.target.value = ""
+		$(".unseen").removeClass("unseen")
+		//  $(".chat").scrollTop($(".chat")[0].scrollHeight)
+		$(".chat").scrollTop(10000)
+	}
 }
