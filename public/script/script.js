bfeb3f7959cc52caf50a01b1e79210fcad5d8b44
Joe Makkio <makkio94@gmail.com>
2019-03-05 13:08:17 +0100

    fixed firebase

--- public/script/script.js
+++ public/script/script.js
@@ -1,24 +1,35 @@
-$(document).ready(() => {
-  $(".chat").scrollTop(100000)
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyDg7OV-1D2cuyUh1oaYFRuMTxqDiI7Zq1U",
+	authDomain: "worksapp-born2code.firebaseapp.com",
+	databaseURL: "https://worksapp-born2code.firebaseio.com",
+	projectId: "worksapp-born2code",
+	storageBucket: "worksapp-born2code.appspot.com",
+	messagingSenderId: "752738389147"
+}
+firebase.initializeApp(config);
 
-  $(".menu-button").click(function() {
-    $("aside").toggle("slide");
-  });
+$(document).ready(() => {
+	$(".chat").scrollTop(100000)
+
+	$(".menu-button").click(function() {
+		$("aside").toggle("slide");
+	});
 })
 
 
 function sendMessage(event) {
-  let c = 0;
-  if (event.which == 13 && event.target.value) {
-    c++
-    var time = new Date().toLocaleTimeString('en-GB', {
-      hour12: false,
-      hour: "numeric",
-      minute: "numeric"
-    });
-    var date = new Date().toLocaleDateString();
-    $(".chat").append(
-      `<article class="chat-row" id=s${c}>
+	let c = 0;
+	if (event.which == 13 && event.target.value) {
+		c++
+		var time = new Date().toLocaleTimeString('en-GB', {
+			hour12: false,
+			hour: "numeric",
+			minute: "numeric"
+		});
+		var date = new Date().toLocaleDateString();
+		$(".chat").append(
+			`<article class="chat-row" id=s${c}>
         <span class="message">
           ${event.target.value}
         </span>
@@ -31,19 +42,19 @@ function sendMessage(event) {
           </div>
         </span>
       </article>`)
-    event.target.value = ""
-    $(".unseen").removeClass("unseen")
-    //  $(".chat").scrollTop($(".chat")[0].scrollHeight)
-    $(".chat").scrollTop(10000)
-  }
+		event.target.value = ""
+		$(".unseen").removeClass("unseen")
+		//  $(".chat").scrollTop($(".chat")[0].scrollHeight)
+		$(".chat").scrollTop(10000)
+	}
 }
 
 
 function logout() {
-  firebase.auth().signOut().then(function() {
-    alert('logout effettuato')
-  }).catch(function(error) {
-    // An error happened
-    alert('errore')
-  })
+	firebase.auth().signOut().then(function() {
+		alert('logout effettuato')
+	}).catch(function(error) {
+		// An error happened
+		alert('errore')
+	})
 }
