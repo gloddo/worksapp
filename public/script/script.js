6162a0b8450413b603e82ae65916a54f08e32fc6
Joe Makkio <makkio94@gmail.com>
2019-03-05 12:28:33 +0100

    Update script.js

--- public/script/script.js
+++ public/script/script.js
@@ -1,24 +1,24 @@
 $(document).ready(() => {
-  $(".chat").scrollTop(100000)
+	$(".chat").scrollTop(100000)
 
-  $(".menu-button").click(function() {
-    $("aside").toggle("slide");
-  });
+	$(".menu-button").click(function() {
+		$("aside").toggle("slide");
+	});
 })
 
 
 function sendMessage(event) {
-  let c = 0;
-  if (event.which == 13 && event.target.value) {
-    c++
-    var time = new Date().toLocaleTimeString('en-GB', {
-      hour12: false,
-      hour: "numeric",
-      minute: "numeric"
-    });
-    var date = new Date().toLocaleDateString();
-    $(".chat").append(
-      `<article class="chat-row" id=s${c}>
+	let c = 0;
+	if (event.which == 13 && event.target.value) {
+		c++
+		var time = new Date().toLocaleTimeString('en-GB', {
+			hour12: false,
+			hour: "numeric",
+			minute: "numeric"
+		});
+		var date = new Date().toLocaleDateString();
+		$(".chat").append(
+			`<article class="chat-row" id=s${c}>
         <span class="message">
           ${event.target.value}
         </span>
@@ -31,7 +31,6 @@ function sendMessage(event) {
           </div>
         </span>
       </article>`)
-<<<<<<< HEAD
 		event.target.value = ""
 		$(".unseen").removeClass("unseen")
 		//  $(".chat").scrollTop($(".chat")[0].scrollHeight)
@@ -40,19 +39,11 @@ function sendMessage(event) {
 }
 
 
-	function logout () { 
-      firebase.auth().signOut().then(function() {
-      alert('logout effettuato')
-    }).catch(function(error) {
-      // An error happened
-      alert('errore')
-      })
-    }
-=======
-    event.target.value = ""
-    $(".unseen").removeClass("unseen")
-    //  $(".chat").scrollTop($(".chat")[0].scrollHeight)
-    $(".chat").scrollTop(10000)
-  }
+function logout() {
+	firebase.auth().signOut().then(function() {
+		alert('logout effettuato')
+	}).catch(function(error) {
+		// An error happened
+		alert('errore')
+	})
 }
->>>>>>> 0e1b12a2b1a6b2f0398a7d95ec42681a1c988f8f
