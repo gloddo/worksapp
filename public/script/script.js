d7f123d99705796a0e6367b9f644f2620c7236cd
ChiaraGallorini <kiara.gal@hotmail.it>
2019-03-06 11:17:22 +0100

    logOut

--- public/script/script.js
+++ public/script/script.js
@@ -1,48 +1,50 @@
 // Initialize Firebase
 var config = {
-	apiKey: "AIzaSyDg7OV-1D2cuyUh1oaYFRuMTxqDiI7Zq1U",
-	authDomain: "worksapp-born2code.firebaseapp.com",
-	databaseURL: "https://worksapp-born2code.firebaseio.com",
-	projectId: "worksapp-born2code",
-	storageBucket: "worksapp-born2code.appspot.com",
-	messagingSenderId: "752738389147"
+  apiKey: "AIzaSyDg7OV-1D2cuyUh1oaYFRuMTxqDiI7Zq1U",
+  authDomain: "worksapp-born2code.firebaseapp.com",
+  databaseURL: "https://worksapp-born2code.firebaseio.com",
+  projectId: "worksapp-born2code",
+  storageBucket: "worksapp-born2code.appspot.com",
+  messagingSenderId: "752738389147"
 }
 firebase.initializeApp(config);
 var db = firebase.firestore();
 
 $(document).ready(() => {
-	$(".chat").scrollTop(100000)
+  $(".chat").scrollTop(100000)
+  document.getElementById("logout").addEventListener("click", logout);
 
-	$(".menu-button").click(function() {
-		$("aside").toggle("slide");
-	});
 
-	$(".p-icon").click(function() {
-		$("#block").toggle("slide", {
-			direction: "down"
-		});
-	});
+  $(".menu-button").click(function() {
+    $("aside").toggle("slide");
+  });
 
-	$(".home").click(function() {
-		$("#block").hide("slide", {
-			direction: "down"
-		});
-	})
+  $(".p-icon").click(function() {
+    $("#block").toggle("slide", {
+      direction: "down"
+    });
+  });
+
+  $(".home").click(function() {
+    $("#block").hide("slide", {
+      direction: "down"
+    });
+  })
 })
 
 
 function sendMessage(event) {
-	let c = 0;
-	if (event.which == 13 && event.target.value) {
-		c++
-		var time = new Date().toLocaleTimeString('en-GB', {
-			hour12: false,
-			hour: "numeric",
-			minute: "numeric"
-		});
-		var date = new Date().toLocaleDateString();
-		$(".chat").append(
-			`<article class="chat-row" id=s${c}>
+  let c = 0;
+  if (event.which == 13 && event.target.value) {
+    c++
+    var time = new Date().toLocaleTimeString('en-GB', {
+      hour12: false,
+      hour: "numeric",
+      minute: "numeric"
+    });
+    var date = new Date().toLocaleDateString();
+    $(".chat").append(
+      `<article class="chat-row" id=s${c}>
         <span class="message">
           ${event.target.value}
         </span>
@@ -55,19 +57,20 @@ function sendMessage(event) {
           </div>
         </span>
       </article>`)
-		event.target.value = ""
-		$(".unseen").removeClass("unseen")
-		//  $(".chat").scrollTop($(".chat")[0].scrollHeight)
-		$(".chat").scrollTop(10000)
-	}
+    event.target.value = ""
+    $(".unseen").removeClass("unseen")
+    //  $(".chat").scrollTop($(".chat")[0].scrollHeight)
+    $(".chat").scrollTop(10000)
+  }
 }
 
+document.getElementById("logout").addEventListener("click", logout);
 
 function logout() {
-	firebase.auth().signOut().then(function() {
-		alert('logout effettuato')
-	}).catch(function(error) {
-		// An error happened
-		alert('errore')
-	})
+  firebase.auth().signOut().then(function() {
+    alert('logout effettuato')
+  }).catch(function(error) {
+    // An error happened
+    alert('errore')
+  })
 }
